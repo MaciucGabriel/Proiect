434436ed1420dd940de85864a4d7da44d30c4139
MaciucGabriel <101754459+MaciucGabriel@users.noreply.github.com>
2022-03-17 00:14:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect
+{
+    class utilizator
+    {
+        //Clasa aceasta va stoca mai multi utilaztori cu urmatoarele atribute:
+        //          -nume
+        //          -parola
+        //          -poza profil
+        //          -data nastere
+        //          -adresa email
+    }
+
+
+    class cheltuieli
+    {
+        //Categorii de cheltuieli:
+        //          -facturi
+        //          -mancare
+        //          -combustibil
+        //          -asigurari
+        //          -cheltuieli neprevazute:
+        //                   ->accidente, sanatate
+        //Va contine grafice pentru cheltuielile fiecarei categorii
+        //Data fiecarei cheltuieli
+    }
+
+    class venituri
+    {
+
+        //Vor exista categorii de venituri active/pasive dar si subcategorii ale acestora:
+        //Grafic procentaj pentru veniturile din anumite perioade
+        
+    }
+
+    class economii
+    {
+        //Clasa va face diferenta dintre venituri si cheltuile
+        //Va contine grafice pentru zilele cu cele mai bune economii
+
+
+    }
+
+   // Toatele clasele vor contine butoane pentru a edita datele acestora dupa adaugarea acestora
+   // De asemenea vor contine butoane pentru a sterge foarte usor anumite entitati
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
